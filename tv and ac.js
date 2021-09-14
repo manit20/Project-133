c8481edf1e0ef91888ef3d0e5bc96ce84bc79511
manit20 <78215866+manit20@users.noreply.github.com>
2021-09-14 19:14:27 +0530

    Add files via upload

---
+++ tv and ac.js
@@ -0,0 +1,45 @@
+status1 = "";
+img = "";
+objects = [];
+
+function preload(){
+    img = loadImage("TV and AC.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(400, 300);
+    canvas.position(445, 270);
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function draw(){
+    image(img, 0, 0, 400, 300);
+
+    if(status1 != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objects Detected";
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status1 = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
